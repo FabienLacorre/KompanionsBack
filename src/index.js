d8d36bc486d558923760ad299fce370c25ef8df5
fabien lacorre <fab.lacorre@gmail.com>
2021-11-21 18:28:35 +0100

    feat: add cors management

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+var cors = require('cors')
+
 const app = express();
 const port = 3000;
 
@@ -10,10 +12,19 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
 
+const corsOptions ={
+  origin: '*',
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
+
 app.get('/pets/dummy', (req, res) => {
+  console.log("PASS IN THE DUMMY REQUEST");
   res.send({
     id: 0,
-    name: 'Dummy',
     userId: 0,
+    name: 'Dummy',
   })
 })
